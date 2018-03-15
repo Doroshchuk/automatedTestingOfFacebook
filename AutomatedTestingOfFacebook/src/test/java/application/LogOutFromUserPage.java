cc540683c502589711ebf9a8435037391bb95573
ddoroshchuk <doroshukdashuly@mail.ru>
2018-03-15 23:30:31 +0200

    refactoring: created class LogOutFromUserPage.

--- AutomatedTestingOfFacebook/src/test/java/application/LogOutFromUserPage.java
+++ AutomatedTestingOfFacebook/src/test/java/application/LogOutFromUserPage.java
@@ -1,18 +1,17 @@
 package application;
 
-import org.testng.annotations.Test;
-
-import pageObjectModels.HomePageModel;
-import pageObjectModels.UserPageModel;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-public class LoginToUserPageTesting {
+import pageObjectModels.HomePageModel;
+import pageObjectModels.UserPageModel;
+
+public class LogOutFromUserPage {
 	private WebDriver driver;
 	private HomePageModel objHomePage;
 	private UserPageModel objUserPage;
@@ -20,18 +19,14 @@ public class LoginToUserPageTesting {
 	private final String baseUrl = "https://uk-ua.facebook.com/";
 	
 	@Test(priority = 0)
-	public void verifyMainPageIsCorrect() {
+	public void testExitFromUserPageIsCorrect() {
 		objHomePage = new HomePageModel(driver);
+		objUserPage = new UserPageModel(driver);
+		objHomePage.loginToFacebook("380968113424", "doroshchuk90697");
+		objUserPage.chooseFunctionInSettingsMenu("¬ийти");
 		Assert.assertTrue(objHomePage.verifyTitle("Facebook Ч ув≥йд≥ть або зареЇструйтес€"));
 	}
 	
-	@Test(priority = 1)
-	public void testUserPageAppearCorrect() {
-		objUserPage = new UserPageModel(driver);
-		objHomePage.loginToFacebook("380968113424", "doroshchuk90697");  
-		Assert.assertTrue(objUserPage.verifyGreetingText("Ћаскаво просимо до Facebook"));
-	}
-  
 	@BeforeClass(alwaysRun = true)
 	public void beforeTest() {
 		ChromeOptions options = new ChromeOptions();
