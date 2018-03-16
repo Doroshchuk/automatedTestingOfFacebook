30605fb2e6434a56441f5f2e84bd6340298a7706
ddoroshchuk <doroshukdashuly@mail.ru>
2018-03-16 19:02:26 +0200

    refactoring: @BeforeClass annotations is added to testing classes.

--- AutomatedTestingOfFacebook/src/test/java/application/LogOutFromUserPage.java
+++ AutomatedTestingOfFacebook/src/test/java/application/LogOutFromUserPage.java
@@ -1,6 +1,7 @@
 package application;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import pageObjectModels.HomePageModel;
@@ -10,10 +11,14 @@ public class LogOutFromUserPage extends BasicTest{
 	private HomePageModel objHomePage;
 	private UserPageModel objUserPage;
 	
-	@Test(priority = 0)
-	public void testExitFromUserPageIsCorrect() {
+	@BeforeClass
+	public void createNecessaryObjects() {
 		objHomePage = new HomePageModel(driver);
 		objUserPage = new UserPageModel(driver);
+	}
+	
+	@Test(priority = 0)
+	public void testExitFromUserPageIsCorrect() {
 		objHomePage.loginToFacebook("380968113424", "doroshchuk90697");
 		objUserPage.chooseFunctionInSettingsMenu("¬ийти");
 		Assert.assertTrue(objHomePage.verifyTitle("Facebook Ч ув≥йд≥ть або зареЇструйтес€"));
--- AutomatedTestingOfFacebook/src/test/java/application/LoginToUserPageTesting.java
+++ AutomatedTestingOfFacebook/src/test/java/application/LoginToUserPageTesting.java
@@ -1,6 +1,7 @@
 package application;
 
 import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import pageObjectModels.HomePageModel;
@@ -11,15 +12,19 @@ public class LoginToUserPageTesting extends BasicTest{
 	private HomePageModel objHomePage;
 	private UserPageModel objUserPage;
 	
+	@BeforeClass
+	public void createNecessaryObjects() {
+		objHomePage = new HomePageModel(driver);
+		objUserPage = new UserPageModel(driver);
+	}
+	
 	@Test(priority = 0)
 	public void verifyMainPageIsCorrect() {
-		objHomePage = new HomePageModel(driver);
 		Assert.assertTrue(objHomePage.verifyTitle("Facebook Ч ув≥йд≥ть або зареЇструйтес€"));
 	}
 	
 	@Test(priority = 1, groups = "correctLogin")
 	public void testUserPageAppearCorrect() {
-		objUserPage = new UserPageModel(driver);
 		objHomePage.loginToFacebook("380968113424", "doroshchuk90697");  
 		Assert.assertTrue(objUserPage.verifyGreetingText("Ћаскаво просимо до Facebook"));
 	}
