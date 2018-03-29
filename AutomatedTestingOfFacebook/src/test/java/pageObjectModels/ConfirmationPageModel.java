ce77d7ac8f32b52ec5d0582a592ac1052c6bfc94
ddoroshchuk <doroshukdashuly@mail.ru>
2018-03-29 13:01:58 +0300

    added constructor

--- AutomatedTestingOfFacebook/src/test/java/pageObjectModels/ConfirmationPageModel.java
+++ AutomatedTestingOfFacebook/src/test/java/pageObjectModels/ConfirmationPageModel.java
@@ -3,6 +3,7 @@ package pageObjectModels;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ConfirmationPageModel {
 	WebDriver driver;
@@ -12,6 +13,11 @@ public class ConfirmationPageModel {
 	
 	@FindBy(xpath = "//div[@class='mts mbl _1v_-']")
 	WebElement messageLabelWithPhoneUser;
+	
+	public ConfirmationPageModel(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(this.driver, this);
+	}
 	
 	public boolean verifyUserName(String expectedName) {
 		String actualUserName = userNameLabel.getText();
